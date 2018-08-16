56d8cb7b1f51e6d6090d13edd5256affefe9f6e4
OgasawaraRyota <carp.nano8727@ezweb.ne.jp>
2018-08-16 21:08:25 +0900

    サンプルテーマ３　初回

---
+++ header 2.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charaset'); ?>">
+<?php wp_head(); ?>
+</head>
+
+<body>
+  <header>
+    <h1>
+      <a href="index.html">
+         <img src="images/logo.gif" width="258" height="64" alt="日本の景色写真展">
+      </a>
+    </h1>
+  </header>
+  <nav>
+
+  </nav>
+
+</body>
+</html>
