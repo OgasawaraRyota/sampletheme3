80f5255c4e543cd06f710612f64cedc8c2dd501c
OgasawaraRyota <carp.nano8727@ezweb.ne.jp>
2018-08-23 20:54:07 +0900

    ロゴ、サイドバー追加

---
+++ footer.php
@@ -0,0 +1,16 @@
+  <footer>
+    <p class="pagetop">
+      <a href="#top">ページの先頭へ戻る</a>
+    </p>
+    <div class="info_field">
+      <address>電話<a href="0120XXX001">　0120-XXX-001</a>
+       FAX 0120-XXX-002</address>
+       <p class="copyright">
+         <small>Copyright 2015日本の景色写真展 All rights reserved.</small>
+       </p>
+     </div>
+  </footer>
+  <script src="<?php echo get_template_directory_uri();?>/js/slideshow.js"></script>
+	<?php wp_footer(); ?>
+</body>
+</html>
